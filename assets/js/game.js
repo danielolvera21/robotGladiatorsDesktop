f188d0944abfffdeb56fdfc0414b2d91c846ed50
Daniel Olvera <danielolvera2188@gmail.com>
2022-09-22 11:52:39 -0600

    added playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("This is an alert! JS is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+function fight() {
+   window.alert('The fight has BEGUN!')
+}
+// fight();
